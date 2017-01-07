ab4cd960b12d6d5c68d1681fd2978b381cb25c05
takayuki.kayawari <kayamelo@gmail.com>
2017-01-07 21:18:12 +0900

    .gitignore_globalと.gitconfigをドットファイルとして管理
    ローカルの自分のPCに依存する自動生成ファイルなどを、gitの追跡に出ないように、グローバルでignoreする.gitignore_globalもドットファイルで管理する。
    ctagsで自動生成されるtagsファイルなどを無視するように設定
    参考
    ・http://qiita.com/elzup/items/4c92a2abdab56db3fb4e
    ・http://qiita.com/katsew/items/5cade12fa743a2f31f25

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,3 +3,4 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
