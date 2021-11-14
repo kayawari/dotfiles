2b97c3211cc9337f745e283883aad7f0b979be6a
takayuki.kayawari <kayamelo@gmail.com>
2021-11-14 22:32:52 +0900

    Delete bash_profile

--- .bash_profile
+++
@@ -1,5 +0,0 @@
-if [ -f ~/.bashrc ] ; then
-    . ~/.bashrc
-fi
-export PATH="/usr/local/sbin:$PATH"
-eval "$(rbenv init -)"
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,4 @@
 ln -si ~/dotfiles/.vimrc ~/.vimrc
-ln -si ~/dotfiles/.bash_profile ~/.bash_profile
 ln -si ~/dotfiles/.vim ~/.vim
 ln -si ~/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/dotfiles/config.fish ~/.config/fish/config.fish
