e01966cb82fe3f696e870ae55655eb4ddb3ed010
takayuki.kayawari <kayamelo@gmail.com>
2021-11-14 22:31:01 +0900

    Delete zshrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,5 @@
 ln -si ~/dotfiles/.vimrc ~/.vimrc
-ln -si ~/dotfiles/.bashrc ~/.bashrc
 ln -si ~/dotfiles/.bash_profile ~/.bash_profile
 ln -si ~/dotfiles/.vim ~/.vim
-ln -si ~/dotfiles/.zshrc ~/.zshrc
 ln -si ~/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/dotfiles/config.fish ~/.config/fish/config.fish
