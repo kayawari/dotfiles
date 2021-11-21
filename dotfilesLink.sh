84bdf4f3b0da00d535aca7851a1170a4d1aeafdb
takayuki.kayawari <kayamelo@gmail.com>
2021-11-21 22:24:36 +0900

    fisherで管理するパッケージもgit管理したいので、pluginsのシンボリックリンクを作る

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,4 @@ ln -si ~/dotfiles/.vimrc ~/.vimrc
 ln -si ~/dotfiles/.vim ~/.vim
 ln -si ~/dotfiles/.gitconfig ~/.gitconfig
 ln -si ~/dotfiles/config.fish ~/.config/fish/config.fish
+ln -si ~/.config/fish/fish_plugins fish_plugins
